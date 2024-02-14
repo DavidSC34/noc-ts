d0c01b6e542def0f52d652fe668fafb8e2bfeca1
David Sánchez Cortez <dsanchez@prototype.mx>
2024-02-14 13:01:24 -0600

    update: leer y crear en mongo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/plugins/envs.plugins';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from "./presentation/server";
 //Ventaja  se espera antes de seguir otroso pasos
 (async()=>{
@@ -6,8 +7,26 @@ main();
 })();
 
 
-function main(){
-    Server.start();
+async function main(){
+
+    await MongoDatabase.connect({
+        mongoUrl:envs.MONGO_URL,
+        dbName:envs.MONGO_DB_NAME
+    })
+
+    //Crear una conleccion = tabla, documento = registro
+    // const newLog = await LogModel.create({
+    //     message:'Test message from Mongo',
+    //     origin:'App.ts',
+    //     level:'low'
+    // });
+
+    // await newLog.save();
+    // console.log(newLog);
+
+    const logs = await LogModel.find();
+    console.log(logs);
+    // Server.start();
 
     // console.log(envs.PORT);
 }
